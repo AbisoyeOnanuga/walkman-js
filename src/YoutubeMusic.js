6f1505c4e5d73c71f87a84d0aae9f161048535db
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2024-11-02 14:04:05 -0400

    install ytmusic-api

---
+++ src/YoutubeMusic.js
@@ -0,0 +1,7 @@
+import ytmusicapi from 'ytmusic-api';
+
+const api = new ytmusicapi({
+  cookie: 'AFmmF2swRgIhAOhcDQJPm6kVmWnKaPJWQfK66gEBRxLNdQ6-4uubWdOJAiEAu4KUl4PU2g0If3ciZEsnkro0hQ9QfIUX2MxxpDvjqTw:QUQ3MjNmeHMyMVpvRWlJREVoYXI0OGRXcFYzWjdTb0x1SjQ5VkpKSjY0Q3VzNUVfTGZjTkFkekhOOTRXd0tVN1FwMUNxeFpENTBDLWIxMWIyVThfcUZGUkpBSmt3emRGcGxVZmhSZFUtSUxCWEIzTDdVS2QwOFFBTUxnTlZCN01vNjVzSTdQQ1VGRUVCdkVsS0R2cUlXQnF1THQ5XzRzTVRn', // You'll need to obtain a valid cookie from YouTube Music
+});
+
+export default api;
