7760a70d06d3ac16bbe85ac893e6e21b385f7b5d
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2024-11-11 03:07:24 -0500

    update themeManager component

--- src/themes/themeManager.ts
+++ src/themes/themeManager.ts
@@ -1,46 +1,21 @@
-// themeManager.ts
-import './Black.css';
-import './Blue.css';
-import './Green.css';
-import './Pink.css';
-import './Red.css';
-import './Yellow.css';
+// context/ThemeContext.js
+import React, { createContext, useState, useContext } from 'react';
+import themes from './themes';
 
-const supportedThemes = {
-  black: "Black",
-  blue: "Blue",
-  green: "Green",
-  pink: "Pink",
-  red: "Red",
-  yellow: "Yellow",
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('silver');
+
+  const changeTheme = (themeName) => {
+    setTheme(themeName);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme: themes[theme], changeTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
 
-export type DeviceThemeName = keyof typeof supportedThemes;
-
-export const setTheme = (theme: DeviceThemeName) => {
-  // Remove all theme classes
-  Object.keys(supportedThemes).forEach(key => {
-    document.body.classList.remove(supportedThemes[key as DeviceThemeName]);
-  });
-  // Add the new theme class
-  document.body.classList.add(supportedThemes[theme]);
-};
-
-/**
- * Initializes the theme based on URL parameters or default logic.
- */
-export const initializeTheme = () => {
-  if (typeof window === "undefined") {
-    return;
-  }
-
-  const params = new URLSearchParams(window.location.search);
-  const themeParam = params.get("theme")?.toLowerCase() as DeviceThemeName;
-
-  if (themeParam && themeParam in supportedThemes) {
-    setTheme(themeParam);
-  } else {
-    // Set a default theme if no valid theme is found in URL
-    setTheme('black'); // Change 'black' to any default theme you prefer
-  }
-};
+export const useTheme = () => useContext(ThemeContext);
