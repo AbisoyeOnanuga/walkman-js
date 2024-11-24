6d17afa29784f8c4146b356c9a8ed1dbf73f0a2b
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2024-11-24 15:32:23 -0500

    update themes js

---
+++ src/utils/themes.js
@@ -0,0 +1,202 @@
+// utils/themes.js
+
+export const Black = {
+  body: {
+    background: "linear-gradient(15deg, #1a1a1a, #4d4d4d)",
+    border: "1px solid #000",
+    backgroundColor: "#494949",
+  },
+  buttonCenter: {
+    background: "linear-gradient(15deg, #1a1a1a, #363636)",
+  },
+  navigation: {
+    background: "linear-gradient(15deg, #1a1a1a, #363636)",
+  },
+  buttonBack: {
+    background: "linear-gradient(15deg, #1a1a1a, #363636)",
+  },
+  buttonOption: {
+    background: "linear-gradient(15deg, #1a1a1a, #363636)",
+  },
+  volumeButtons: {
+    background: "linear-gradient(15deg, #1a1a1a, #363636)",
+  },
+  powerButton: {
+    background: "linear-gradient(15deg, #1a1a1a, #363636)",
+  },
+  keyButton: {
+    background: "linear-gradient(15deg, #292929, #585858)",
+  },
+  active: {
+    buttonCenter: {
+      backgroundColor: "rgba(0, 0, 0, 0.5)"
+    },
+    buttonUp: {
+      background: "linear-gradient(0deg, #2e2e2e, #1a1a1a)",
+      width: "120px"
+    },
+    buttonDown: {
+      background: "linear-gradient(180deg, #2020208b, #090909)",
+      width: "120px"
+    },
+    buttonLeft: {
+      background: "linear-gradient(270deg, #26262644, #1a1a1a)",
+      height: "120px",
+      top: "15px",
+      width: "30px"
+    },
+    buttonRight: {
+      background: "linear-gradient(90deg, #26262644, #1a1a1a)",
+      height: "120px",
+      top: "15px",
+      width: "30px"
+    }
+  }
+};
+  
+export const Blue = {
+  body: {
+    background: "linear-gradient(15deg, #4682b4, #1e90ff)",
+  },
+  buttonCenter: {
+    background: "linear-gradient(15deg, #4682b4, #1e90ff)",
+  },
+  navigation: {
+    background: "linear-gradient(15deg, #4682b4, #1e90ff)",
+  },
+  buttonBack: {
+    background: "linear-gradient(15deg, #4682b4, #1e90ff)",
+  },
+  buttonOption: {
+    background: "linear-gradient(15deg, #4682b4, #1e90ff)",
+  },
+  volumeButtons: {
+    background: "linear-gradient(15deg, #4682b4, #1e90ff)",
+  },
+  powerButton: {
+    background: "linear-gradient(15deg, #4682b4, #1e90ff)",
+  },
+  keyButton: {
+    background: "linear-gradient(15deg, #4682b4, #1e90ff)",
+  },
+};
+  
+export const Pink = {
+  body: {
+    background: "linear-gradient(15deg, #ff1493, #ff69b4)",
+  },
+  buttonCenter: {
+    background: "linear-gradient(15deg, #ff1493, #ff69b4)",
+  },
+  navigation: {
+    background: "linear-gradient(15deg, #ff1493, #ff69b4)",
+  },
+  buttonBack: {
+    background: "linear-gradient(15deg, #ff1493, #ff69b4)",
+  },
+  buttonOption: {
+    background: "linear-gradient(15deg, #ff1493, #ff69b4)",
+  },
+  volumeButtons: {
+    background: "linear-gradient(15deg, #ff1493, #ff69b4)",
+  },
+  powerButton: {
+    background: "linear-gradient(15deg, #ff1493, #ff69b4)",
+  },
+  keyButton: {
+    background: "linear-gradient(15deg, #ff1493, #ff69b4)",
+  },
+};
+
+export const Red = {
+  body: {
+    background: "linear-gradient(15deg, #ff4500, #ff6347)",
+  },
+  buttonCenter: {
+    background: "linear-gradient(15deg, #ff4500, #ff6347)",
+  },
+  navigation: {
+    background: "linear-gradient(15deg, #ff4500, #ff6347)",
+  },
+  buttonBack: {
+    background: "linear-gradient(15deg, #ff4500, #ff6347)",
+  },
+  buttonOption: {
+    background: "linear-gradient(15deg, #ff4500, #ff6347)",
+  },
+  volumeButtons: {
+    background: "linear-gradient(15deg, #ff4500, #ff6347)",
+  },
+  powerButton: {
+    background: "linear-gradient(15deg, #ff4500, #ff6347)",
+  },
+  keyButton: {
+    background: "linear-gradient(15deg, #ff4500, #ff6347)",
+  },
+};
+
+export const Yellow = {
+  body: {
+    background: "linear-gradient(15deg, #ff8c00 , #ffd700)",
+  },
+  buttonCenter: {
+    background: "linear-gradient(15deg, #ff8c00 , #ffd700)",
+  },
+  navigation: {
+    background: "linear-gradient(15deg, #ff8c00 , #ffd700)",
+  },
+  buttonBack: {
+    background: "linear-gradient(15deg, #ff8c00 , #ffd700)",
+  },
+  buttonOption: {
+    background: "linear-gradient(15deg, #ff8c00 , #ffd700)",
+  },
+  volumeButtons: {
+    background: "linear-gradient(15deg, #ff8c00 , #ffd700)",
+  },
+  powerButton: {
+    background: "linear-gradient(15deg, #ff8c00 , #ffd700)",
+  },
+  keyButton: {
+    background: "linear-gradient(15deg, #ff8c00 , #ffd700)",
+  },
+};
+
+export const Green = {
+  body: {
+    background: "linear-gradient(15deg, #228b22, #32cd32)",
+  },
+  buttonCenter: {
+    background: "linear-gradient(15deg, #228b22, #32cd32)",
+  },
+  navigation: {
+    background: "linear-gradient(15deg, #228b22, #32cd32)",
+  },
+  buttonBack: {
+    background: "linear-gradient(15deg, #228b22, #32cd32)",
+  },
+  buttonOption: {
+    background: "linear-gradient(15deg, #228b22, #32cd32)",
+  },
+  volumeButtons: {
+    background: "linear-gradient(15deg, #228b22, #32cd32)",
+  },
+  powerButton: {
+    background: "linear-gradient(15deg, #228b22, #32cd32)",
+  },
+  keyButton: {
+    background: "linear-gradient(15deg, #228b22, #32cd32)",
+  },
+};
+
+export const themes = {
+  Black,
+  Blue,
+  Pink,
+  Red,
+  Yellow,
+  Green,
+  // Add other themes here
+};
+  
+export default themes;
