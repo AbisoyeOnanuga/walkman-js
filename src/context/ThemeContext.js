64dde7c9a166387681cdf98427a9b6416397249b
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2024-12-01 00:26:12 -0500

    update ThemeContext js, save theme in localstorage

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,15 +1,24 @@
 // context/ThemeContext.js
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 import { themes } from '../utils/themes';
 
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('Black'); // Default theme
+  // Retrieve the stored theme or default to 'Black'
+  const storedTheme = localStorage.getItem('theme') || 'Black';
+  const [theme, setTheme] = useState(storedTheme);
 
   const changeTheme = (themeName) => {
     setTheme(themeName);
+    localStorage.setItem('theme', themeName); // Store the selected theme in localStorage
   };
+
+  useEffect(() => {
+    // Update the theme on initial load
+    const storedTheme = localStorage.getItem('theme') || 'Black';
+    setTheme(storedTheme);
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ theme: themes[theme], changeTheme }}>
