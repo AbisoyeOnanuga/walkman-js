83345501cc9d92135c5bc6878f86e49e3ea92f60
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2024-12-06 23:08:40 -0500

    update playbackscreen component

--- src/components/PlaybackScreen.jsx
+++ src/components/PlaybackScreen.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import MusicScreen from './MusicScreen';
 import RadioScreen from './RadioScreen';
 import PlaylistsScreen from './PlaylistsScreen';
+import Header from './Header';
 
 const PlaybackScreen = ({ playing }) => {
-  return playing === 'music' ? <MusicScreen setPlaying={() => {}} /> : <RadioScreen setPlaying={() => {}} />;
+  return (
+    <div className="playback-screen">
+      <Header playing={playing} />
+      <div className="screen-content">
+        {playing === 'music' ? <MusicScreen setPlaying={() => {}} /> : <RadioScreen setPlaying={() => {}} />}
+      </div>
+    </div>
+  );
 };
 
 export default PlaybackScreen
