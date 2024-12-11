2f3943a0b48deeeb62063f32f53549e15463733a
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2024-12-10 23:02:18 -0500

    update playlistsscreen component

--- src/components/PlaylistsScreen.jsx
+++ src/components/PlaylistsScreen.jsx
@@ -1,31 +1,127 @@
 import React, { useEffect } from 'react';
+import Header from './Header';
+import './PlaylistsScreen.css';
 
-const PlaylistsScreen = ({ setPlaying, playing }) => {
+const PLAYLISTS = [
+  {
+    id: 'playlist1',
+    title: 'Lofi Loft Mix',
+    videoId: 'rKGRuQBnatM',
+    listId: 'RDCLAK5uy_kb7EBi6y3GrtJri4_ZH56Ms786DFEimbM',
+    thumbnail: 'https://i.ytimg.com/vi/rKGRuQBnatM/default.jpg'
+  },
+  {
+    id: 'playlist2',
+    title: 'Take It Easy Rock',
+    videoId: 'SebH8En9ZOY',
+    listId: 'RDCLAK5uy_mfut9V_o1n9nVG_m5yZ3ztCif29AHUffI',
+    thumbnail: 'https://i.ytimg.com/vi/SebH8En9ZOY/default.jpg'
+  },
+  {
+    id: 'playlist3',
+    title: 'Lofi Hip Hop',
+    videoId: 'jfKfPfyJRdk',
+    thumbnail: 'https://i.ytimg.com/vi/jfKfPfyJRdk/default.jpg'
+  },
+  {
+    id: 'playlist4',
+    title: 'Classical Mix',
+    videoId: 'mIYzp5rcTvU',
+    thumbnail: 'https://i.ytimg.com/vi/mIYzp5rcTvU/default.jpg'
+  },
+  {
+    id: 'playlist5',
+    title: 'Jazz Vibes',
+    videoId: 'Dx5qFachd3A',
+    thumbnail: 'https://i.ytimg.com/vi/Dx5qFachd3A/default.jpg'
+  },
+  {
+    id: 'playlist6',
+    title: 'Piano Relaxation',
+    videoId: '77ZozI0rw7w',
+    thumbnail: 'https://i.ytimg.com/vi/77ZozI0rw7w/default.jpg'
+  }
+];
+
+const PlaylistsScreen = ({ 
+  playing, 
+  setPlaying, 
+  currentPlaylist, 
+  onPlaylistPlay, 
+  setYoutubePlayer 
+}) => {
   useEffect(() => {
-    if (playing === 'playlists') {
-      // Logic to resume playing content if previously playing
+    if (!window.YT) {
+      const tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
+      const firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
     }
-  }, [playing]);
+
+    const initPlayer = () => {
+      const player = new window.YT.Player('youtube-player', {
+        height: '100%',
+        width: '100%',
+        videoId: PLAYLISTS[0].videoId,
+        playerVars: {
+          autoplay: 0,
+          controls: 1,
+          modestbranding: 1,
+          rel: 0,
+          playsinline: 1,
+          list: PLAYLISTS[0].listId
+        },
+        events: {
+          onReady: (event) => {
+            setYoutubePlayer(event.target);
+          },
+          onStateChange: (event) => {
+            if (event.data === window.YT.PlayerState.PLAYING) {
+              setPlaying('playlists');
+            } else if (event.data === window.YT.PlayerState.PAUSED || 
+                     event.data === window.YT.PlayerState.ENDED) {
+              setPlaying(null);
+            }
+          }
+        }
+      });
+    };
+
+    if (window.YT && window.YT.Player) {
+      initPlayer();
+    } else {
+      window.onYouTubeIframeAPIReady = initPlayer;
+    }
+
+    return () => {};
+  }, [setYoutubePlayer, setPlaying]);
 
   return (
     <div className="playlists-screen">
-      <div className="header">
-        <div className="status-icons">
-          <div className="play-pause"></div>
-          <div className="song-status"></div>
-        </div>
-        <div className="battery-icon"></div>
-      </div>
+      <Header playing={playing} />
       <div className="screen-content">
-        <iframe
-          width="100%" height="100%"
-          src="https://www.youtube.com/embed/rKGRuQBnatM?list=RDCLAK5uy_kb7EBi6y3GrtJri4_ZH56Ms786DFEimbM"
-          title="why dont we"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin" allowFullScreen
-          onLoad={() => setPlaying('playlists')}
-        ></iframe>
+        <div className="player-container" style={{ visibility: 'visible' }}>
+          <div id="youtube-player"></div>
+        </div>
+        <div className="playlists-list">
+          {PLAYLISTS.map(playlist => (
+            <div 
+              key={playlist.id}
+              className={`playlist ${currentPlaylist?.id === playlist.id ? 'active' : ''}`}
+              onClick={() => onPlaylistPlay(playlist)}
+            >
+              <div className="playlist-thumbnail">
+                <img src={playlist.thumbnail} alt={playlist.title} />
+              </div>
+              <div className="playlist-info">
+                <div className="playlist-title">{playlist.title}</div>
+              </div>
+              <div className="playlist-status">
+                {currentPlaylist?.id === playlist.id ? '▶' : ''}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
